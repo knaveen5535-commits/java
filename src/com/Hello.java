1fcd7c4040e91a51360c94f23f9b4053056ea659
[200~ <knaveen5535@gmail.com>
2026-08-22 12:07:15 +0530

    normal commit

--- src/com/Hello.java
+++ src/com/Hello.java
@@ -2,13 +2,13 @@ package src.com;
 
 import chennai.X;
 
-public class Hello extends X {
+public class Hello {
    
    public static void main(String[] args) {
-      Hello obj = new Hello();
+       X obj = new X();
       //System.out.println("private: " + obj.b);
       //System.out.println("Short Value: " + obj.s);
-      System.out.println("Protected: " + obj.i);
+     // System.out.println("Protected: " + obj.i);
       System.out.println("Public: " + obj.l);
    }
 
