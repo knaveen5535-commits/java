467a1e033f2aa56bc70b65e0150fb8b9e7d52e4d
[200~ <knaveen5535@gmail.com>
2026-08-22 11:34:07 +0530

    learn to run the code

--- src/com/Hello.java
+++ src/com/Hello.java
@@ -1,16 +1,22 @@
 package src.com;
 public class Hello {
+   
+   public static void main(String[] args) {
+      X obj = new X();
+      //System.out.println("Byte Value: " + obj.b);
+      System.out.println("Short Value: " + obj.s);
+      System.out.println("Int Value: " + obj.i);
+      System.out.println("Long Value: " + obj.l);
+   }
+
+   
+}
+
+class X
+{
    private byte b=5;
       short s=10;
    protected int i=15547777;
    public long l=2085622458l;
 
-   public static void main(String[] args) {
-      Hello obj = new Hello();
-      System.out.println("Byte Value: " + obj.b);
-      System.out.println("Short Value: " + obj.s);
-      System.out.println("Int Value: " + obj.i);
-      System.out.println("Long Value: " + obj.l);
-   }
-   
 }
