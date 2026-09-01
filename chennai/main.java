20b75fe98a8bb30de6d0c2c8d5b0b8acf649c8a1
[200~ <knaveen5535@gmail.com>
2026-09-01 10:34:32 +0530

    Refactor thread implementation to use MethodDemo class with enhanced thread properties

--- chennai/main.java
+++ chennai/main.java
@@ -1,32 +1,21 @@
 package chennai;
 
-class mythread implements Runnable
-{
-    public void run ()
-    {
-        for(int i = 0; i<= 9; ++i)
-        {
-            if (i==5)
-            {
-                try
-                {
-                    Thread.sleep(5000);
-                }
-                catch (Exception e)
-                {
-                    System.out.println(e.toString());
-                }
-            }
-            
-            System.out.println(i+1);
-        }
-    }
-    public static void main(String args[])
-    {
-        mythread mt = new mythread();
-        System.out.println("Before starting thread");
-        Thread t = new Thread(mt);
-        t.start();
-        
+class MethodDemo extends Thread{
+    public static void main(String args[]){
+        MethodDemo t1=new MethodDemo();
+        System.out.println("Default thread name is: "+t1.getName());
+        t1.setName("Hello Thread");
+        System.out.println("t1 name after assigning :"+t1.getName());
+        System.out.println("Default thread priority is: "+t1.getPriority());
+        t1.setPriority(Thread.MAX_PRIORITY);
+        System.out.println("t1 priority after assigning :"+t1.getPriority());
+        System.out.println("Default thread group is: "+t1.getThreadGroup());
+        System.out.println("Default t1 isDaemon thread is: "+t1.isDaemon());
+        t1.setDaemon(true);
+        System.out.println("After assigning t1 isDaemon thread is: "+t1.isDaemon());
+        System.out.println("Default t1 isAlive thread is: "+t1.isAlive());
+        System.out.println("Default t1 isInterrupted thread is: "+t1.isInterrupted());
+        System.out.println("name of the current thread is: "+Thread.currentThread());
+
     }
 }
