ffdeff6a4feb1a4884a33adc2a6b11dcde56d0df
[200~ <knaveen5535@gmail.com>
2026-09-02 11:10:47 +0530

    trim function in the string

--- chennai/StringDemo2.java
+++ chennai/StringDemo2.java
@@ -3,11 +3,14 @@ package chennai;
 public class StringDemo2 {
     public static void main(String[] args)
     {
-        String s = "Naveen from coimbatore";
-        System.out.println(s);
-        System.out.println(s.substring(8));
-        System.out.println(s.substring(8, 14));
-        System.out.println(s.indexOf( 'a'));
-        System.out.println(s.lastIndexOf('a'));
+        String s = " Naveen from coimbatore ";
+    //     System.out.println(s);
+    //     System.out.println(s.substring(8));
+    //     System.out.println(s.substring(8, 14));
+    //     System.out.println(s.indexOf( 'a'));
+    //     System.out.println(s.lastIndexOf('a'));
+        System.out.println(s.length());
+        s= s.trim();
+        System.out.println(s.length());
     }
 }
