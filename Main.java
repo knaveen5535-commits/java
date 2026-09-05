19b66e2ad0224eb22a245421d3f59f3a8e9d9675
[200~ <knaveen5535@gmail.com>
2026-09-05 10:40:08 +0530

    reference in data structure

---
+++ Main.java
@@ -0,0 +1,11 @@
+class student {
+    int id;
+    
+}
+public class Main {
+    public static void main(String[] args) {
+        student s1 = new student();
+        s1.id = 101;
+        System.out.println("Student ID: " + s1.id);
+    }
+}
